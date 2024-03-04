de70404831d13a0c9bd456ff476386d769e62bde
presi300 <eruditezenith@protonmail.com>
2024-03-04 15:58:58 +0200

    fixed small bug

--- src/app/Chapters/DownloadAndSetupPage/page.js
+++ src/app/Chapters/DownloadAndSetupPage/page.js
@@ -1,8 +1,8 @@
 "use client";
 import React from "react";
-import { MainFmt } from "../components/FormattedText/Fmt";
-import Text from "../components/Text/Text";
-import Skeleton from "../components/Skeleton/Skeleton";
+import { MainFmt } from "../../components/FormattedText/Fmt";
+import Text from "../../components/Text/Text";
+import Skeleton from "../../components/Skeleton/Skeleton";
 
 export default function DwAndSetup({}) {
   return (
